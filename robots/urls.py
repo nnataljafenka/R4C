4be346e5f4cb6007b5e1212085942ce6286a2b1d
Natalia <nataljafenka@gmail.com>
2023-10-04 22:43:11 +0800

    1 try

--- robots/urls.py
+++ robots/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'create_robots'
 urlpatterns = [
-    path('create_robot/', views.RobotCreateView.as_view(), name='create_robots'),
+    path('create_robot/', views.RobotCreateView.as_view(), name='create_robot'),
+    path('report/', views.RobotReportView.as_view(), name='robot_report'),
+    path('download_report/', views.RobotReportView.download_report, name='download_report'),
 ]
