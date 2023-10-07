eb5aaa507dc1289322c2573ed694e719bf0f7c9e
Natalia <nataljafenka@gmail.com>
2023-10-07 19:16:25 +0800

    3 задача: обновила работу со статусами

--- orders/signals.py
+++ orders/signals.py
@@ -11,18 +11,22 @@ def robot_available_notification(sender, instance, **kwargs):
     pass
     subject = "Робот доступен"
     message = f"Добрый день!\nНедавно вы интересовались нашим роботом модели {instance.model}, версии {instance.version}.\nЭтот робот теперь в наличии. Если вам подходит этот вариант, пожалуйста, свяжитесь с нами."
-    recipient_list = [order.customer.email for order in Order.objects.filter(robot_serial=instance.serial, status='AD')]
-    send_mail(subject=subject, message=message, from_email='testmaildjango87@mail.ru', recipient_list=recipient_list)
+    orders = Order.objects.filter(robot_serial=instance.serial, status='AD')
+    recipient_list =[order.customer.email for order in orders]
+    send_mail(subject=subject, message=message, recipient_list=recipient_list,from_email='testmaildjango87@mail.ru')
+    for order in orders:
+        order.status = 'AC'
+        order.save()
 
 
 @receiver(pre_save, sender=Order)
 def create_order(sender, instance, **kwargs):
-    robot = Robot.objects.filter(serial=instance.robot_serial).first()
-    if not robot:
-        subject = "Робот не доступен"
-        message = f"Добрый день!\n Заказанный робот не в наличии. При поступлении робота на склад, мы оповестим вас."
-        recipient_list = instance.customer.email
-        send_mail(subject=subject, message=message, from_email='testmaildjango87@mail.ru',
-                  recipient_list=recipient_list)
-        instance.status = "AD"
-        instance.save()
+    if instance.status == 'C':
+        robot = Robot.objects.filter(serial=instance.robot_serial).first()
+        if not robot:
+            subject = "Робот не доступен"
+            message = f"Добрый день!\n Заказанный робот не в наличии. При поступлении робота на склад, мы оповестим вас."
+            recipient_list = instance.customer.email
+            send_mail(subject=subject, message=message, recipient_list=recipient_list,from_email='testmaildjango87@mail.ru')
+            instance.status = "AD"
+            instance.save()
