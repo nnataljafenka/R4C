72bb8df8cf32229157b824716b66aa7fff0e34df
Natalia <71044858+nnataljafenka@users.noreply.github.com>
2023-10-07 22:01:10 +0800

    Update views.py

--- robots/views.py
+++ robots/views.py
@@ -1,37 +1,35 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 from django.views import View
+from django.views.generic import TemplateView
 from .models import Robot
 from .serializers import RobotSerializer
 import json
+import openpyxl
+from openpyxl.styles import Font, PatternFill
+from openpyxl.utils import get_column_letter
 import datetime
 
 
 # Create your views here.
 class RobotCreateView(View):
     def post(self, request):
-        try:
-            data = json.loads(request.body.decode('utf-8'))
-            serializer = RobotSerializer(data)
-            if serializer.is_valid():
-                # Проверка на наличие модели
-                existing_robot = Robot.objects.filter(serial=serializer.serial).first()
-                if existing_robot:
-                    return JsonResponse({'message': 'Robot have already existed'}, status=400)
-
-                serializer.save()
-
-                response_data = {
-                    'message': 'Robot created successfully!',
-                    'robot_data': {
-                        'serial': serializer.serial,
-                        'model': serializer.model,
-                        'version': serializer.version,
-                        'created': serializer.created.strftime("%Y-%m-%d %H:%M:%S")
-                    }
+        robot = RobotSerializer()
+        robot.parse_json(request)
+        if robot.is_valid():
+            robot.save()
+            response_data = {
+                'message': 'Robot created successfully!',
+                'robot_data': {
+                    'serial': robot.serial,
+                    'model': robot.model,
+                    'version': robot.version,
+                    'created': robot.created.strftime("%Y-%m-%d %H:%M:%S")
                 }
-                return JsonResponse(response_data, status=201)
-            else:
-                return JsonResponse({'error': 'Invalid JSON format or data. Valid format: {"model":"model_name","version":"version_name","created":"date"}, format date:"%Y-%m-%d %H:%M:%S"'}, status=400)
-        except (json.JSONDecodeError, ValueError, TypeError, KeyError, OverflowError):
-            return HttpResponseBadRequest('Invalid JSON data')
+            }
+            return JsonResponse(response_data, status=201)
+        else:
+            return JsonResponse({
+                'error': 'Invalid JSON format or data. Valid format: {"model":"model_name","version":"version_name","created":"date"}, format date:"%Y-%m-%d %H:%M:%S"'},
+                status=400)
+
