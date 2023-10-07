21d3a77fdd20e904ceff9ea93a9153ec6206d712
Natalia <71044858+nnataljafenka@users.noreply.github.com>
2023-10-07 22:08:29 +0800

    Update views.py

--- robots/views.py
+++ robots/views.py
@@ -14,33 +14,24 @@ import datetime
 # Create your views here.
 class RobotCreateView(View):
     def post(self, request):
-        try:
-            data = json.loads(request.body.decode('utf-8'))
-            serializer = RobotSerializer(data)
-            if serializer.is_valid():
-                # Проверка на наличие модели
-                existing_robot = Robot.objects.filter(serial=serializer.serial).first()
-                if existing_robot:
-                    return JsonResponse({'message': 'Robot have already existed'}, status=400)
-
-                serializer.save()
-
-                response_data = {
-                    'message': 'Robot created successfully!',
-                    'robot_data': {
-                        'serial': serializer.serial,
-                        'model': serializer.model,
-                        'version': serializer.version,
-                        'created': serializer.created.strftime("%Y-%m-%d %H:%M:%S")
-                    }
+        robot = RobotSerializer()
+        robot.parse_json(request)
+        if robot.is_valid():
+            robot.save()
+            response_data = {
+                'message': 'Robot created successfully!',
+                'robot_data': {
+                    'serial': robot.serial,
+                    'model': robot.model,
+                    'version': robot.version,
+                    'created': robot.created.strftime("%Y-%m-%d %H:%M:%S")
                 }
-                return JsonResponse(response_data, status=201)
-            else:
-                return JsonResponse({
-                    'error': 'Invalid JSON format or data. Valid format: {"model":"model_name","version":"version_name","created":"date"}, format date:"%Y-%m-%d %H:%M:%S"'},
-                    status=400)
-        except (json.JSONDecodeError, ValueError, TypeError, KeyError, OverflowError):
-            return HttpResponseBadRequest('Invalid JSON data')
+            }
+            return JsonResponse(response_data, status=201)
+        else:
+            return JsonResponse({
+                'error': 'Invalid JSON format or data. Valid format: {"model":"model_name","version":"version_name","created":"date"}, format date:"%Y-%m-%d %H:%M:%S"'},
+                status=400)
 
 
 class RobotReportView(TemplateView):
